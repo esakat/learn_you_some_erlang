beb98ed324d9c63703464813ab5cf65db54ec99e
esakat <esaka.tom@gmail.com>
2018-07-12 08:27:35 +0900

    In case of

--- src/chap3/chap3.erl
+++ src/chap3/chap3.erl
@@ -20,7 +20,9 @@
   right_age/1,
   heh_fine/0,
   oh_god/1,
-  help_me/1
+  help_me/1,
+  insert/2,
+  beach/1
 ]).
 
 %% パターンマッチ
@@ -105,3 +107,25 @@ help_me(Animal) ->
 %% Erlangのif-elseはelseをtrueで表現してそれ以外全てをで表現する
 %% あまりこれは推奨されない(わかりづらいし)
 %% 条件を明記するのが好ましい P41.参照
+
+%% In case .. of
+%% パターンマッチとガードで表現できるけどね
+insert(X, []) ->
+  [X];
+insert(X, Set) ->
+  case lists:member(X, Set) of
+    true -> Set;
+    false -> [X|Set]
+  end.
+
+beach(Temperature) ->
+  case Temperature of
+    {celsius, N} when N >= 20, N =< 45 ->
+      'favorable';
+    {kelvin, N} when N >= 293, N =< 318 ->
+      'scientifically favorable';
+    {fahrenheit, N} when N >= 68, N =< 113 ->
+      'favorable in the US';
+    _ ->
+      'avoid beach'
+  end.
